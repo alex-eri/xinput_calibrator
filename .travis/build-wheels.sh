683dcb1d2fe8c10e91c1ea27fee81232333238cc
Aleksandr Stepanov <sa@eri.su>
2021-05-25 12:38:23 +0300

    3x

--- .travis/build-wheels.sh
+++ .travis/build-wheels.sh
@@ -15,7 +15,7 @@ function repair_wheel {
 #yum install -y atlas-devel
 
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
+for PYBIN in /opt/python/3.*/bin; do
     "${PYBIN}/pip" install -r /io/dev-requirements.txt
     "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
 done
